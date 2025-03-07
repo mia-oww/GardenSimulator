0300f8de853da7d29fc90944979f5db1a7059b71
miaow <miafendi01@gmail.com>
2025-03-07 17:57:58 -0500

    Add files via upload

---
+++ Garden.java
@@ -0,0 +1,25 @@
+/*
+THIS CODE WAS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING
+CODE WRITTEN BY OTHER STUDENTS OR COPIED FROM ONLINE RESOURCES. _MIA_GOMEZ_
+*/
+public class Garden {
+
+    private Gardener[] gardeners;
+    private int gardenerCount;
+
+    public Garden(int size) {
+        gardeners = new Gardener[size];
+        gardenerCount = 0;
+    }
+
+    public void addGardener(Gardener gardener) { // to add more gardeners to garden
+        if (gardenerCount < gardeners.length) {
+            gardeners[gardenerCount++] = gardener;
+        } else {
+            System.out.println("Garden is full! Cannot add more gardeners.");
+        }
+    }
+    public int getGardenerCount() {
+        return gardenerCount;
+    }
+}
